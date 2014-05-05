ab7078a2ee87bd37a0990c12a9300d602e52d456
marcusthomaswong@gmail.com <marcusthomaswong@gmail.com@5da95816-2985-11df-b785-935fb9e1f515>
2014-05-05 11:24:11 +0000

    Add new 'legacy RSS' plugin - I use it for custom RSS feeds on my gallery instances

---
+++ trunk/legacy-rss.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Legacy RSS
+ *
+ * Brings back the legacy RSS functionality of Zenphoto
+ *
+ * @author Marcus Wong (wongm)
+ * @package plugins
+ */
+
+$plugin_description = gettext("Brings back the legacy RSS functionality of Zenphoto.");
+$plugin_author = "Marcus Wong (wongm)";
+$plugin_version = '1.0.0'; 
+$plugin_URL = "http://code.google.com/p/wongm-zenphoto-plugins/";
+
+require_once(dirname(__FILE__).'/legacy-rss/functions-rss.php');
+
+?>
