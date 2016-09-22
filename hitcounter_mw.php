d74f1a15965075e4f88a2fc1850103bf4449e721
marcusw <marcusw@pageuppeople.com>
2016-09-22 12:36:27 +1000

    Move hitcounter date reset logic into plugin

--- hitcounter_mw.php
+++ hitcounter_mw.php
@@ -291,4 +291,32 @@ function getRollingHitcounter($obj, $galleryType="", $splitLines=true)
 	}
 	return $toreturn;
 }
+
+function updateHitcounterDates()
+{
+    updateHitcounterDatesFor('images');
+    updateHitcounterDatesFor('albums');
+}
+
+function updateHitcounterDatesFor($type)
+{
+	$time = time();
+	$dateCurrent = date("Y-m-d");
+	$dateLastWeek = date("Y-m-d", $time - (60*60*24*6.5));
+	$dateLastMonth = date("Y-m-d", $time - (60*60*24*30));
+	
+	// ensure all new images have the reset date set to today
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_month_reset = '$dateCurrent' WHERE hitcounter_month_reset IS NULL";
+	query($sqlToReset);
+	
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_week_reset = '$dateCurrent' WHERE hitcounter_week_reset IS NULL";
+	query($sqlToReset);
+	
+	// reset the week / month hitcounter if required
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_month_reset = '$dateCurrent', hitcounter_month = 0 WHERE hitcounter_month_reset < '$dateLastMonth'";
+	query($sqlToReset);
+	
+	$sqlToReset = "UPDATE " . prefix($type) . " SET hitcounter_week_reset = '$dateCurrent', hitcounter_week = 0 WHERE hitcounter_week_reset < '$dateLastWeek'";
+	query($sqlToReset);
+}
 ?>
